0c5e36d4ba21872714c3b047abd936048f17696d
ofcljaved <javedofficialme@gmail.com>
2024-12-11 17:39:51 +0530

    chore: improve code readablity

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,16 @@
 use std::io::{self, Write};
 
-fn get_numeric_input(prompt: &str) -> Option<u8> {
-    print!("=> ");
-    print!("{}", prompt);
-    print!(" : ");
+fn get_input<T: std::str::FromStr>(prompt: &str) -> Result<T, String> {
+    print!("=> {} : ", prompt);
     io::stdout().flush().ok();
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
-        .expect("Failed to read the line");
-    match input.trim().parse() {
-        Ok(value) => Some(value),
-        Err(_) => {
-            println!("Inavlid input, enter a number");
-            None
-        }
-    }
-}
-
-fn get_string_input(prompt: &str) -> String {
-    print!("=> ");
-    print!("{}", prompt);
-    print!(" : ");
-    io::stdout().flush().ok();
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read the line");
-    input.trim().to_string()
+        .map_err(|_| "Input Error: Failed to read line")?;
+    input
+        .trim()
+        .parse::<T>()
+        .map_err(|_| "Input Error: Invalid Input".to_string())
 }
 
 fn main() {
@@ -44,19 +27,31 @@ fn main() {
         println!("| 3. Show Task              |");
         println!("| 4. Exit                   |");
         println!("-----------------------------");
-        let choice = match get_numeric_input("Enter your choice") {
-            Some(value) => value,
-            None => continue,
+        let choice: u8 = match get_input("Enter your choice") {
+            Ok(value) => value,
+            Err(err) => {
+                println!("{}", err);
+                continue;
+            }
         };
         match choice {
             1 => {
-                let description = get_string_input("Enter Task description");
+                let description: String = match get_input("Enter Task description") {
+                    Ok(desc) => desc,
+                    Err(err) => {
+                        println!("{}", err);
+                        continue;
+                    }
+                };
                 todo_app.add_task(&description);
             }
             2 => {
-                let index = match get_numeric_input("Enter Task index to toggle it's completion") {
-                    Some(value) => value as usize,
-                    None => continue,
+                let index: usize = match get_input("Enter Task index to toggle it's completion") {
+                    Ok(value) => value,
+                    Err(err) => {
+                        println!("{}", err);
+                        continue;
+                    }
                 };
                 todo_app.update_task(index - 1);
             }
@@ -79,6 +74,11 @@ impl Task {
             completed: false,
         }
     }
+
+    fn display(&self, id: usize) {
+        let status = if self.completed { "[X]" } else { "[]" };
+        println!("| {:<2}  | {:<18}  | {}", id, status, self.description);
+    }
 }
 
 struct Todo {
@@ -87,11 +87,6 @@ struct Todo {
 
 fn display_task_header() {
     println!("| Id  | Status (completed)  | Description  ");
-}
-
-fn display_format(id: usize, completed: bool, desc: &str) {
-    let status = if completed { "[X]" } else { "[]" };
-    println!("| {}  | {}  | {}", id, status, desc);
 }
 
 impl Todo {
@@ -104,29 +99,23 @@ impl Todo {
         self.tasks.push(task);
         let last_task_added = self.tasks.last().unwrap();
         display_task_header();
-        display_format(
-            self.tasks.len(),
-            last_task_added.completed,
-            &last_task_added.description,
-        );
+        last_task_added.display(self.tasks.len());
     }
 
     fn update_task(&mut self, index: usize) {
         if let Some(task) = self.tasks.get_mut(index) {
             task.completed = !task.completed;
             display_task_header();
-            display_format(
-                index  + 1,
-                task.completed,
-                &task.description,
-            );
+            task.display(index + 1);
+        } else {
+            println!("Inavlid task index");
         }
     }
 
     fn display_task(&mut self) {
         display_task_header();
         for (index, task) in self.tasks.iter().enumerate() {
-            display_format(index + 1, task.completed, &task.description);
+            task.display(index + 1);
         }
     }
 }
